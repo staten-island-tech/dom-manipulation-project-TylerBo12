d152e642a094713bd7d7d42d1da10c5d49d68f9d
TylerBo12 <Tylershovel@gmail.com>
2022-11-03 13:14:54 -0400

    I'm confused? I'll do more later??

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ const DOMselectors = {
   food: document.querySelectorAll(`.food`),
 };
 console.log(DOMselectors.food);
+
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = `red`;
+  text.textContent = `This a biiiiiiiiiiiiiig red box`;
+  text.style.fontSize = `7rem`;
+  text.style.margin = `1rem`;
+}
+
+backgroundAndText(DOMselectors.button, DOMselectors.text);
+
+DOMselectors.button.addEventListener(`click`, function () {
+  backgroundAndText(DOMselectors.button, DOMselectors.text);
+});
