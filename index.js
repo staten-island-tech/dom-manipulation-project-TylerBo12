2006177cdafee449ceb035df71411e3eec56882c
TylerBo12 <Tylershovel@gmail.com>
2022-11-03 22:23:34 -0400

    I took notes and...

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-console.log(`connected`);
+/* console.log(`connected`);
 
 const DOMselectors = {
   button: document.getElementById(`btn`),
-  text: document.querySelector(`#text`),
+  text: document.querySelector(`.text`),
   empty: document.querySelector(`.nothing`),
   food: document.querySelectorAll(`.food`),
+  container: document.querySelector(`.container`),
 };
 console.log(DOMselectors.food);
 
@@ -15,8 +16,31 @@ function backgroundAndText(background, text) {
   text.style.margin = `1rem`;
 }
 
-backgroundAndText(DOMselectors.button, DOMselectors.text);
+DOMselectors.button.addEventListener("click", function () {
+  backgroundAndText(DOMselectors.container, DOMselectors.text);
+});
 
-DOMselectors.button.addEventListener(`click`, function () {
-  backgroundAndText(DOMselectors.button, DOMselectors.text);
+function changeLi(food) {
+  let foodIndex = 1;
+  DOMselectors.food.forEach((el) => {
+    el.addEventListener(`click`, function () {
+      el.textContent = `Hello ${foodIndex}`;
+      foodIndex++;
+    });
+  });
+}
+
+changeLi(); */
+
+const DOMselectors2 = {
+  button: document.querySelector(`#btn2`),
+  box: document.querySelector(`#container-box`),
+  input: document.querySelector(`#input`),
+};
+const cat = `meow`;
+DOMselectors2.box.insertAdjacentHTML(`beforebegin`, `This is an ${cat}`);
+DOMselectors2.button.addEventListener(`click`, function () {
+  let input = DOMselectors2.input.value;
+  DOMselectors2.box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
+  DOMselectors2.input.value = "";
 });
