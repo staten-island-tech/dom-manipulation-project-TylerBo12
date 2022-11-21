0a166e0c4340ed0719189d6196c45f6f5e74ac55
TylerBo12 <Tylershovel@gmail.com>
2022-11-20 23:09:14 -0500

    I hate everything.

--- index.js
+++ index.js
@@ -14,19 +14,30 @@ DOMselectors.button.addEventListener("click", function () {
   const input1 = DOMselectors.input1.value;
   const img = DOMselectors.img.value;
   const input2 = DOMselectors.input2.value;
-  const button = "button";
 
-  DOMselectors.objectBox.insertAdjacentHTML(
-    "beforeend",
-    `<div class = "objectXbox"> <p>${input1}</p> <p><img src= ${img} class="imgView"></p> <p>${input2}</p>  <button id="tempBtn">${DOMselectors.button2.innerHTML}</button></div>`
-  );
-  /* DOMselectors.input1.value = "";
-  DOMselectors.img.value = "";
-  DOMselectors.input2.value = ""; */
+  function makeObject() {
+    DOMselectors.objectBox.insertAdjacentHTML(
+      "beforeend",
+      `<div class = "objectXbox"> <p>${input1}</p> <p><img src= ${img} class="imgView"></p> <p>${input2}</p>  <button class="tempBtn">Remove</button></div>`
+    );
+    DOMselectors.input1.value = "";
+    DOMselectors.img.value = "";
+    DOMselectors.input2.value = "";
+  }
+  function erase() {
+    const remove = document.querySelectorAll(".tempBtn");
+    remove.forEach((eachRemove) => {
+      eachRemove.addEventListener("click", (event) => {
+        event.target.parentElement.remove();
+      });
+    });
+  }
+  makeObject();
+  erase();
 });
 
-DOMselectors.button2.addEventListener(`click`, function (e) {
+/* DOMselectors.button2.addEventListener(`click`, function (e) {
   e.preventDefault();
   DOMselectors.objectBox.innerHTML = "";
   console.log("hi");
-});
+}); */
